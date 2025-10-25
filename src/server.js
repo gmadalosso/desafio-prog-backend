500b8c2e6799060f88ce1e06b0f546ff108ff414
gmadalosso <gbmadalosso@gmail.com>
2025-10-25 19:15:43 -0300

    criada conexão com o banco de dados

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import pool from "./config/database.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+
+try {
+  const connection = await pool.getConnection();
+  console.log("Conexão com o MySQL concluída com sucesso!");
+  connection.release();
+} catch (error) {
+  console.error("Erro ao conectar ao MySQL:", error);
+}
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
