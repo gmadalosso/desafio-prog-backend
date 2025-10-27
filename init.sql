929820c46024fd6f0f83171ecbc7667fb3260208
gmadalosso <gbmadalosso@gmail.com>
2025-10-27 14:22:57 -0300

    Atualizadas tabelas do banco

--- init.sql
+++ init.sql
@@ -1,31 +1,35 @@
+-- Cria tabela de usuários
 CREATE TABLE IF NOT EXISTS usuarios (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      username VARCHAR(50) UNIQUE NOT NULL,
-      password_hash VARCHAR(255) NOT NULL,
-      role ENUM('aluno', 'professor') NOT NULL
-      );
-      
-      CREATE TABLE IF NOT EXIST disciplinas (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        nome VARCHAR(100) NOT NULL,
-        codigo VARCHAR(10) NOT NULL,
-        professor_id INT,
-        FOREIGN KEY (professor_id) REFERENCES usuarios(id)
-      );
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nomeUsuario VARCHAR(50) UNIQUE NOT NULL,
+  senha_hash VARCHAR(255) NOT NULL,
+  papel INT NOT NULL  -- 0 = aluno, 1 = professor
+);
 
-      CREATE TABLE IF NOT EXISTS turmas (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        disciplina_id INT,
-        horario VARCHAR(50),
-        vagas_disponiveis INT DEFAULT 30,
-        FOREIGN KEY (disciplina_id) REFERENCES disciplinas(id)
-      );
+-- Cria tabela de disciplinas
+CREATE TABLE IF NOT EXISTS disciplinas (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  codigo VARCHAR(10) NOT NULL,
+  professor_id INT,
+  FOREIGN KEY (professor_id) REFERENCES usuarios(id)
+);
 
-      CRATE TABLE IF NOT EXISTS matriculas (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        aluno_id INT,
-        turma_id INT,
-        status ENUM('ativa', 'cancelada') DEFAULT 'ativa',
-        FOREIGN KEY (aluno_id) REFERENCES usuarios(id),
-        FOREIGN KEY (turma_id) REFERENCES turmas(id)
-      );
+-- Cria tabela de turmas
+CREATE TABLE IF NOT EXISTS turmas (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  disciplina_id INT,
+  horario VARCHAR(50),
+  vagas_disponiveis INT DEFAULT 30,
+  FOREIGN KEY (disciplina_id) REFERENCES disciplinas(id)
+);
+
+-- Cria tabela de matrículas
+CREATE TABLE IF NOT EXISTS matriculas (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  aluno_id INT,
+  turma_id INT,
+  status INT DEFAULT 0,  -- 0 = ativa, 1 = cancelada
+  FOREIGN KEY (aluno_id) REFERENCES usuarios(id),
+  FOREIGN KEY (turma_id) REFERENCES turmas(id)
+);
