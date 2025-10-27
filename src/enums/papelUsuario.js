dd4c25d12ed04a2dcf6c1de6d458168b542a9187
gmadalosso <gbmadalosso@gmail.com>
2025-10-27 15:06:02 -0300

    criados enums de papeis de usuário

---
+++ src/enums/papelUsuario.js
@@ -0,0 +1,9 @@
+export const PAPEL_USUARIO = {
+  ALUNO: 0,
+  PROFESSOR: 1
+};
+
+export const PAPEL_USUARIO_LABELS = {
+  [PAPEL_USUARIO.ALUNO]: 'Aluno',
+  [PAPEL_USUARIO.PROFESSOR]: 'Professor'
+};
