2b1e1f8344bd8d67df3fa754036c1caffa5254cb
gmadalosso <gbmadalosso@gmail.com>
2025-11-06 00:44:48 -0300

    script limpa banco

---
+++ limpar_banco.sql
@@ -0,0 +1,21 @@
+-- Script para limpar todas as tabelas e resetar AUTO_INCREMENT
+-- Executar este script no MySQL antes de reiniciar o servidor
+
+USE sistema_matriculas;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+TRUNCATE TABLE matriculas;
+TRUNCATE TABLE turmas;
+TRUNCATE TABLE disciplinas;
+TRUNCATE TABLE usuarios;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+ALTER TABLE usuarios AUTO_INCREMENT = 1;
+ALTER TABLE disciplinas AUTO_INCREMENT = 1;
+ALTER TABLE turmas AUTO_INCREMENT = 1;
+ALTER TABLE matriculas AUTO_INCREMENT = 1;
+
+SELECT 'Tabelas limpas com sucesso!' as status;
+
